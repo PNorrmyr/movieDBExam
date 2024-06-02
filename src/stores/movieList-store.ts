0de72a6432fcc221e3703621899bc79b7789534e
PNorrmyr <philip.norrmyr@gmail.com>
2024-06-02 10:46:49 +0200

    added movieListupdate on delete

--- src/stores/movieList-store.ts
+++ src/stores/movieList-store.ts
@@ -27,16 +27,18 @@ const useMovieListStore = create<MovieStore>((set) => ({
         movieList : [...movies]
        })
     },
-
-    deleteMovie : (movieId : string, apiKey : string) => {
+    
+    deleteMovie : (movieId: string, apiKey: string) => {
         axios.delete(`http://localhost:8080/api/movies/${movieId}?key=${apiKey}`)
         .then(response => {
-            console.log(response.data);
+            set(state => ({movieList: state.movieList.filter(movie => movie.imdbid !== movieId)
+            }));
+            console.log("Deleted successfully:", response.data);
         })
-        
+        .catch(error => {
+            console.log("Error deleting movie:", error);
+        });
 
-    }
-
-}))
+}}))
 
 export default useMovieListStore
